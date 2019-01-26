e63d4bed97386d7b3de537ca76303e352b8d5c0d
sharicodes <srosenb@yahoo.com>
2019-01-26 13:55:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400){
+  return "This one is on me!"
+
+} else if (someValue >2000) {
+  if (someValue >2500) {
+    return 'No can do.'
+  } else {
+  return 'I will gladly take your thirty bucks.'
+
+}
+};
+return response;
+}
+
+function ternaryCheckCity(city){
+const response = city === 'NYC' ? "Ok, sounds good." : "No go.";
+return response;
+}
+function switchOnCharmFromTip(tip_amt) {
+  if (tip_amt === "generous"){
+    return "Thank you so much."
+  } else if (tip_amt === "not as generous"){
+    return "Thank you."
+  } else {
+    return "Bye."
+  }
+}
