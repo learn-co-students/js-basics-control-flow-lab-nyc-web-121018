625a6199dce05d344eb8e8ac626ff8a72f3eecdb
Oli Nelson <olivernelson@icloud.com>
2019-01-26 19:00:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+function scuberGreetingForFeet(number){
+  if (number < 400) {
+    return 'This one is on me!'
+  }
+  else if (number > 2000 && number < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else  {
+    return 'No can do.'
+  }
+
+}
+
+function ternaryCheckCity(city) {
+  return city ==="NYC" ? 'Ok, sounds good.' : 'No go.';
+}
+
+
+function switchOnCharmFromTip(quality){
+  switch (quality) {
+    case "generous":
+      return "Thank you so much."
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+  }
+}
